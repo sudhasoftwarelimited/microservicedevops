18bcf098062a9f9bb1551201ab1bcf59af13627a
Kondayya Devarakonda <sudhasoftwarelimited.com>
2025-06-04 16:03:30 +0100

    Modified InvoiceCreateController

--- src/main/java/com/microservicedevops/api/controller/InvoiceCreateController.java
+++ src/main/java/com/microservicedevops/api/controller/InvoiceCreateController.java
@@ -1,4 +1,15 @@
 package com.microservicedevops.api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/microservicedevops/api/v1")
 public class InvoiceCreateController {
+    @GetMapping("/invoice")
+    public String invoice ()
+    {
+        return "Welcome to Invoice application";
+    };
 }
